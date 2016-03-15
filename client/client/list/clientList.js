0daea748a1a70401bbd2b7a18c68fe2b3e953b15
Grigory Tsvetkov <gr.tsvetkov@gmail.com>
2016-03-15 18:28:22 +0300

    a 0.2

--- client/client/list/clientList.js
+++ client/client/list/clientList.js
@@ -3,6 +3,8 @@ var oTable1;
 
 Template.clientList.rendered = function () {
 
+
+
     var rusLng = {
         "processing": "Подождите...",
         "search": "Поиск:",
@@ -28,6 +30,51 @@ Template.clientList.rendered = function () {
 
     jQuery(function ($) {
 
+
+        jQuery.extend( jQuery.fn.dataTableExt.oSort, {
+            "date-eu-pre": function ( date ) {
+                date = date.replace(" ", "");
+
+                if ( ! date ) {
+                    return 0;
+                }
+
+                var year;
+                var eu_date = date.split(/[\.\-\/]/);
+
+                /*year (optional)*/
+                if ( eu_date[2] ) {
+                    year = eu_date[2];
+                }
+                else {
+                    year = 0;
+                }
+
+                /*month*/
+                var month = eu_date[1];
+                if ( month.length == 1 ) {
+                    month = 0+month;
+                }
+
+                /*day*/
+                var day = eu_date[0];
+                if ( day.length == 1 ) {
+                    day = 0+day;
+                }
+
+                return (year + month + day) * 1;
+            },
+
+            "date-eu-asc": function ( a, b ) {
+                return ((a < b) ? -1 : ((a > b) ? 1 : 0));
+            },
+
+            "date-eu-desc": function ( a, b ) {
+                return ((a < b) ? 1 : ((a > b) ? -1 : 0));
+            }
+        });
+
+
         //initiate dataTables plugin
         oTable1 =
             $('#dynamic-table')
@@ -35,10 +82,11 @@ Template.clientList.rendered = function () {
                 .dataTable({
                     language: rusLng,
                     bAutoWidth: false,
+                    columnDefs: [
+                        null, null, null, { type: 'date-eu', targets: 0 }, null, null, null, null
+                    ],
                     "aoColumns": [
-                        //{"bSortable": false},
-                        null, null, {"bSortable": false}, null, null, null, null,
-                        {"bSortable": false}
+                        null, null, null, null, null, null, null, {"bSortable": false}
                     ],
                     "aaSorting": [],
 
@@ -222,9 +270,9 @@ Template.clientListItem.helpers({
         }
     },
 
-    'lastEvent_date': function(_id) {
+    'lastEventDate': function(_id) {
 
-        return moment(Event.find({'data.client_id': _id}, {sort: {'dt': -1}}).fetch()[0].dt).format('DD.MM.YYYY hh:mm');
+        return moment(Event.find({'data.client_id': _id}, {sort: {'dt': -1}}).fetch()[0].dt).format('DD.MM.YYYY');
     }
 
 
